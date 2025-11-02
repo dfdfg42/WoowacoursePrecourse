f43e4e5dfcba31137490da1a9d4b21167f56371b
dfdfg42 <dfdfg11@gmail.com>
2025-11-02 15:17:47 +0900

    Feat: 로또 숫자 범위 검증 기능 추가

--- java-lotto-8/src/test/java/lotto/LottoTest.java
+++ java-lotto-8/src/test/java/lotto/LottoTest.java
@@ -21,4 +21,26 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 숫자는 1~45 사이여야 합니다 (45초과)")
+    @Test
+    void 로또_번호가_45_를_벗어나면_예외가_발생한다(){
+        //given 준비
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 46);
+
+        //when then
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 숫자는 1~45 사이여야 합니다 (0인경우")
+    @Test
+    void 로또_번호가_0_를_벗어나면_예외가_발생한다(){
+        //given 준비
+        List<Integer> numbers = List.of(0, 2, 3, 4, 5, 6, 7);
+
+        //when then
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
