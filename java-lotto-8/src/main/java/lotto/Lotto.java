5ea7737207787c44251a5c08588d04a592488ff4
dfdfg42 <dfdfg11@gmail.com>
2025-11-02 15:11:44 +0900

    Feat: 로또 중복 검증 기능 추가

--- java-lotto-8/src/main/java/lotto/Lotto.java
+++ java-lotto-8/src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +15,11 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if(numbersSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있을 수 없습니다.");
         }
     }
 
