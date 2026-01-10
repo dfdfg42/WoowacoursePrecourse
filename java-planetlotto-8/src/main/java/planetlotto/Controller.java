cd34d2dfdd4ca93c4f059882e893d5f8f8f2dab6
dfdfg42 <dfdfg11@gmail.com>
2026-01-10 16:49:36 +0900

    feat: 입력 멈춤처리

--- java-planetlotto-8/src/main/java/planetlotto/Controller.java
+++ java-planetlotto-8/src/main/java/planetlotto/Controller.java
@@ -44,13 +44,17 @@ public class Controller {
                 //당첨 결과 저장
                 Statistics stats = new Statistics(purchasedLottos, winningLotto.getLotto(), bonusNumber.getNumber());
                 Map<Rank, Integer> rankCounts = stats.getRankCounts();
+
+
                 HashMap<Integer, Integer> hashMap = new HashMap<>();
+                System.out.println(rankCounts.get(Rank.FIFTH));
                 hashMap.put(1, rankCounts.get(Rank.FIRST));
                 hashMap.put(2, rankCounts.get(Rank.SECOND));
                 hashMap.put(3, rankCounts.get(Rank.THIRD));
                 hashMap.put(4, rankCounts.get(Rank.FOURTH));
                 hashMap.put(5, rankCounts.get(Rank.FIFTH));
                 outputView.printResult(hashMap);
+                break;
 
 
             } catch (IllegalArgumentException e) {
