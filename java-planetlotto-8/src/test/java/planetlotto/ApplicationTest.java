4647f058ef4613389cb829a476cc8abba7f950d3
dfdfg42 <dfdfg11@gmail.com>
2026-01-10 13:53:29 +0900

    feat: 구매 기능 예외 테스트

--- java-planetlotto-8/src/test/java/planetlotto/ApplicationTest.java
+++ java-planetlotto-8/src/test/java/planetlotto/ApplicationTest.java
@@ -3,12 +3,15 @@ package planetlotto;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -61,5 +64,35 @@ class ApplicationTest extends NsTest {
         // then
         assertThat(purchase.getAmountAsInt()).isEqualTo(3000);
         assertThat(purchase.getLottoCount()).isEqualTo(6); // 3000원 = 3장
+    }
+    @DisplayName("입력값이 숫자가 아닐 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", " ", "1000a"})
+    void 숫자가_아닌_입력값은_예외가_발생한다(String input) {
+        // when & then
+        assertThatThrownBy(() -> new Purchase(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력값이 500원 단위가 아닐 경우 예외가 발생한다.")
+    void 천원_단위가_아닌_금액은_예외가_발생한다() {
+        // given
+        String input = "1200"; // 500으로 나누어 떨어지지 않음
+
+        // when & then
+        assertThatThrownBy(() -> new Purchase(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력값이 500원 미만일 경우 예외가 발생한다.")
+    void 천원_미만의_금액은_예외가_발생한다() {
+        // given
+        String input = "0"; // 1000원 미만
+
+        // when & then
+        assertThatThrownBy(() -> new Purchase(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
